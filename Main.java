efebc8a047225686c169efc00100441f091727bd
Kyllian <kyllianfranceschi@gmail.com>
2022-09-06 14:36:47 +0200

    salut

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main
 {
     public static void main (String args []){
-        System.out.println("Hello form main");
+        System.out.println("Hello form main from git002");
         Pangolin p =new Pangolin("toto", 5);
         System.out.println(p.crier());
     }
