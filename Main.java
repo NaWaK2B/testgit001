c4482c9262194a9e1af27cf613e7178764543427
Kyllian <kyllianfranceschi@gmail.com>
2022-09-06 11:06:09 +0200

    C2: ajout de pangolin.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main (String args []){
+        System.out.println("Hello form main");
+        Pangolin p =new Pangolin("toto", 5);
+        System.out.println(p.crier());
+    }
+}
